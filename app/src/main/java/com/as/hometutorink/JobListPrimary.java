f5259f7d9ca4432b10d9c0629342d93f6dd76911
atiqah.shariff <atiqah.shariff@aemenersol.com>
2019-03-15 06:02:52 +0800

    + finish all ui

---
+++ app/src/main/java/com/as/hometutorink/JobListPrimary.java
@@ -0,0 +1,13 @@
+package com.as.hometutorink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobListPrimary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_list_primary);
+    }
+}
---
+++ app/src/main/java/com/as/hometutorink/TutorAcceptJobsPrimary.java
@@ -0,0 +1,13 @@
+package com.as.hometutorink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorAcceptJobsPrimary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_accept_jobs_primary);
+    }
+}
