937e2ddcea8925e1e95d1999d7cde98af435da3c
atiqah.shariff <atiqah.shariff@aemenersol.com>
2019-03-25 01:41:49 +0800

    New UI

---
+++ app/src/main/java/com/as/hometutorink/ParentHistoryPost.java
@@ -0,0 +1,13 @@
+package com.as.hometutorink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParentHistoryPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_history_post);
+    }
+}
