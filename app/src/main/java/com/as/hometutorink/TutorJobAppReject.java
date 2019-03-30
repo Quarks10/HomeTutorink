c0c5d5ffc97f3f829d3aff81286e7edfe45956c2
atiqah.shariff <atiqah.shariff@aemenersol.com>
2019-03-30 22:30:13 +0800

    Job Application Pending, Rejected and Cards

---
+++ app/src/main/java/com/as/hometutorink/TutorJobAppReject.java
@@ -0,0 +1,13 @@
+package com.as.hometutorink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorJobAppReject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_job_app_reject);
+    }
+}
